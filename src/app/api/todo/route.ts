5157439d07be4e9e9e643c28e63c8ea28d10a947
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-05-25 21:54:22 +0500

    after post function

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -3,13 +3,34 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const client = await db.connect()
+    
 
     try {
         await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255))`
         return NextResponse.json({message:"Todos Table created With Two Fields Id and Task"}); 
     } catch (error) {
-        return NextResponse.json({message: "something went wrong"})
+        return NextResponse.json({message: (error as {message: string}).message})
+    }
+    
+}
+
+
+export async function POST(request: NextRequest) {
+    const client = await db.connect()
+    const req = await request.json()
+    
+    try {
+        if (req.task) {
+            await client.sql`INSERT INTO Todos(Task) VALUE(${req.task})`
+            
+        } else {
+            throw new Error("Task field is required")
+        }
+        
+    } catch (error) {
+        return NextResponse.json({message: (error as {message: string}).message})
+        
     }
 
-
 }
+
