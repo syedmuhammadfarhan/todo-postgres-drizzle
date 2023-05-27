dcd5c300936035de4dd7b21ff85a629285fa5219
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-05-27 18:06:26 +0500

    backend

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,14 +1,16 @@
-import { db } from "@vercel/postgres";
+// import { db } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
+import { db, todoTable } from "@/lib/drizzle"
+import { sql } from "@vercel/postgres"
 
 export async function GET(request: NextRequest) {
-    const client = await db.connect()
-    
-
     try {
-        await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255))`
-        return NextResponse.json({message:"Todos Table created With Two Fields Id and Task"}); 
+        await sql`CREATE TABLE IF NOT EXISTS Todos(id serial primary key, Task varchar(255))`
+
+        const res = await db.select().from(todoTable)
+        return NextResponse.json({ message: "Todos Table created With Two Fields Id and Task" });        
     } catch (error) {
+        console.log((error as {message: string}).message)
         return NextResponse.json({message: (error as {message: string}).message})
     }
     
@@ -16,13 +18,14 @@ export async function GET(request: NextRequest) {
 
 
 export async function POST(request: NextRequest) {
-    const client = await db.connect()
     const req = await request.json()
     
     try {
         if (req.task) {
-            await client.sql`INSERT INTO Todos(Task) VALUE(${req.task})`
-            
+             const res = await db.insert(todoTable).values({
+                task: req.task,
+            }).returning()
+            return NextResponse.json({ message: "Data added successfully" })
         } else {
             throw new Error("Task field is required")
         }
